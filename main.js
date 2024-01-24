d7ba2fcf0aa72596dbea46ed1b23f30716c0acae
Priyanka <pbhaukajee@gmail.com>
2024-01-24 16:01:47 -0600

    add homepage.js and export info to index.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hi");
+(()=>{"use strict";!function(){const e=document.querySelector("#content"),t=document.createElement("div");t.classList.add("home-right");const n=document.createElement("h1");n.textContent="Indulge in the world of sweet delights!";const o=document.createElement("p");o.textContent="Join us – where every bite tells a story, and every moment is a celebration of sweetness. Welcome to our bakery family!",console.log("Title element:",n),console.log("Paragraph element:",o),e.appendChild(t),t.append(n,o)}(),console.log("Hi")})();
